cf2c692e01b67d17f2afdffac612ccfe6cd5ee31
Konrad Oleksiuk <konole@gmail.com>
2020-03-25 19:59:11 +0100

    Initial structure

---
+++ cw2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceFile = args[0];
+            var outputFile = args[1];
+            var format = args[2];
+
+            Console.WriteLine($"Source File: {sourceFile}, output file: {outputFile}, format: {format}");
+
+            if (!File.Exists(sourceFile))
+            {
+                Console.WriteLine("File does not exist");
+            }
+
+            List<Student> students = new List<Student>();
+
+            var file = File.Open(sourceFile, FileMode.Open);
+
+            using (var stream = new StreamReader(File.OpenRead(sourceFile)))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] student = line.Split(','); var st = new Student
+                    {
+                        firstName = student[0],
+                        lastName = student[1]
+                    };
+                }
+            }
+
+            Console.WriteLine($"{students}");
+        }
+
+        private static Exception FileNotFoundException(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cw2/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace cw2
+{
+    public struct Student
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String course { get; set; }
+        public String typeOfCourse { get; set; }
+        public int number { get; set; }
+        public DateTime birthdate { get; set; }
+        public String email { get; set; }
+        public String fatherName { get; set; }
+        public String motherName { get; set; }
+    }
+}
